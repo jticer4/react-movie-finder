b50876299fea61129e940164dbfcb9b3f53e6dd0
jticer4 <jticer@cnm.edu>
2018-08-09 16:31:35 -0600

    added my title and logo with some styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React Movie Finder</h1>
+        <table style={{
+          backgroundColor: '#000',
+           display: 'block',
+           color: '#fff'
+        }}>
+          <tbody>
+            <tr>
+              <td>
+					  <img width="50" src="https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" alt=""/>
+              </td>
+              <td>
+                MoviesDB Search
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
