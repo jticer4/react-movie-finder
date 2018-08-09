2046415f270b3f45ada81c73c9df263942c61d08
jticer4 <jticer@cnm.edu>
2018-08-09 16:38:47 -0600

    made the font size of the title larger and enclosed it in a h3 element

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <table style={{
           backgroundColor: '#000',
            display: 'block',
-           color: '#fff'
+           color: '#fff',
+           fontSize: 24
         }}>
           <tbody>
             <tr>
@@ -17,7 +18,7 @@ class App extends Component {
 					  <img width="50" src="https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" alt=""/>
               </td>
               <td>
-                MoviesDB Search
+                <h3>MoviesDB Search</h3>
               </td>
             </tr>
           </tbody>
