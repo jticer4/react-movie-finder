fc12f40d873246812e4be9cfb1845746131fa5bc
jticer4 <jticer@cnm.edu>
2018-08-10 17:37:47 -0600

    refactored my indentation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <table className="titleBar">
-          <tbody>
-            <tr>
-              <td>
-					  <img width="50" src="https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" alt=""/>
-              </td>
-					<td width="8"/>
-              <td>
-                <h1>MoviesDB Search</h1>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<table className="titleBar">
+					<tbody>
+						<tr>
+							<td>
+								<img width="50"
+									  src="https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg"
+									  alt=""/>
+							</td>
+							<td width="8"/>
+							<td>
+								<h1>MoviesDB Search</h1>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 
 export default App;
