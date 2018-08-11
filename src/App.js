74a8dc33b02379138e11ef5f94de8c7ff73dfb61
jticer4 <jticer@cnm.edu>
2018-08-10 18:20:26 -0600

    added my input field and added some styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 							<td>
 								<img width="50"
 									  src="https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg"
-									  alt=""/>
+									  alt="app icon"/>
 							</td>
 							<td width="8"/>
 							<td>
@@ -20,6 +20,13 @@ class App extends Component {
 						</tr>
 					</tbody>
 				</table>
+				<input style={{
+					fontSize: 24,
+					display: 'block',
+					width: "99%",
+					paddingTop: 8,
+					paddingBottom: 8
+				}} type="text" placeholder="Enter search term"/>
 			</div>
 		);
 	}
