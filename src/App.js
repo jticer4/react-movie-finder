445637264b5f3197e2e1aa892f5f9ba56b000b53
jticer4 <jticer@cnm.edu>
2018-08-13 22:58:45 -0600

    added in my api key and consule logged the first result of the query using ajax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import MovieRow from './MovieRow.js'
+import $ from 'jquery'
 
 class App extends Component {
 	constructor(props) {
@@ -8,32 +9,49 @@ class App extends Component {
 		//set the state to an empty object
 		this.state = {}
 		//console.log("this is my initializer")
-/*
-		//my array of dummy data
-		const movies = [
-			{id: 0, poster_src: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lDlGPZS0UJYKxVlpyff3BMyPc2H.jpg", title: "Mean Girls", overview: "some filler text!!!"},
-			{id: 1, poster_src: "https://image.tmdb.org/t/p/w185_and_h278_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Infinity War", overview: "the ending is so sad :("}
-		]
+		/*
+				//my array of dummy data
+				const movies = [
+					{id: 0, poster_src: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lDlGPZS0UJYKxVlpyff3BMyPc2H.jpg", title: "Mean Girls", overview: "some filler text!!!"},
+					{id: 1, poster_src: "https://image.tmdb.org/t/p/w185_and_h278_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Infinity War", overview: "the ending is so sad :("}
+				]
 
 
-		//holds my array of movies
-		var movieRows = []
+				//holds my array of movies
+				var movieRows = []
 
-		//for each movie in the movies array add it to the movieRows array
-		movies.forEach((movie) => {
-			const movieRow = <MovieRow movie={movie}/>
+				//for each movie in the movies array add it to the movieRows array
+				movies.forEach((movie) => {
+					const movieRow = <MovieRow movie={movie}/>
 
-			//push each movieRow onto the movieRows array
-			movieRows.push(movieRow)
-		})
+					//push each movieRow onto the movieRows array
+					movieRows.push(movieRow)
+				})
 
-		//assign the movieRows array to the rows state variable
-		this.state = {rows: movieRows}*/
+				//assign the movieRows array to the rows state variable
+				this.state = {rows: movieRows}*/
 		this.performSearch()
 	}
 
 	performSearch() {
 		console.log("perform search using movies db")
+		//stores the api key
+		const urlString = "https://api.themoviedb.org/3/search/collection?api_key=d9b5150291e4eb66066d423a4d75f4b7&language=en-US&query=mean+girls&page=1"
+		$.ajax({
+			url: urlString,
+			success: (searchResults) => {
+				console.log("data fetched successfully")
+
+				//stores the results from the query
+				const results = searchResults.results
+
+				//console log the first result in the response
+				console.log(results[0])
+			},
+			error: (xhr, status, err) => {
+				console.error("data fetched unsuccessfully")
+			}
+		})
 	}
 
 	render() {
